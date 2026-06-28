9f5d84974e49a0f3f359512fee060b14ef164ad8
yehoshua <y0587828030@gmail.com>
2026-06-28 14:07:16 +0300

    Square calculation function 1

--- toolkit.py.py
+++ toolkit.py.py
@@ -4,7 +4,12 @@
 #     print(f"hello, {name}")
 # greet("agent x")
 
-#step 2
-def add(a,b):
-    print(a + b)
-add(3, 4)
+# #step 2
+# def add(a,b):
+#     print(a + b)
+# add(3, 4)
+
+#step 3
+def square(n):
+    print(n**2)
+square(5)
