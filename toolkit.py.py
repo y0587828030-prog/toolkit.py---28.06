f7c8daee79a442b4f91367365b75bb99221286d4
yehoshua <y0587828030@gmail.com>
2026-06-28 13:54:35 +0300

    Task 1 - Function Hello

---
+++ toolkit.py.py
@@ -0,0 +1,12 @@
+#פונקציה נכונה
+# def master(msg):
+#     ver = input(msg)
+#     print(ver.lower())
+# master("enter name ")
+# master("enter add ")
+
+#Part 1
+#step 1
+def greet(name):
+    print(f"hello, {name}")
+greet("agent x")
