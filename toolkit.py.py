a9228869a514e1568b43e5d1f748a3ebe6554112
yehoshua <y0587828030@gmail.com>
2026-06-28 15:17:28 +0300

    Task 7 The high number function

--- toolkit.py.py
+++ toolkit.py.py
@@ -14,25 +14,37 @@
 #     print(n**2)
 # square(12)
 
-#step 4
-def greet_with_title(name, title= "agent"):
-    print("hello",title, name)
-greet_with_title("yehosh")
+# #step 4
+# def greet_with_title(name, title= "agent"):
+#     print("hello",title, name)
+# greet_with_title("yehosh")
 
-def greet_with_title(name, title="agent"):
-    print("hello",title, name)
-greet_with_title("yehosh", "commander")
+# def greet_with_title(name, title="agent"):
+#     print("hello",title, name)
+# greet_with_title("yehosh", "commander")
 
-#step 5
-def describe(name, level, active):
-    print("name:", name, "level:", level , "active:" ,active )
-describe(name="agent x.", active= True, level=5)
+# #step 5
+# def describe(name, level, active):
+#     print("name:", name, "level:", level , "active:" ,active )
+# describe(name="agent x.", active= True, level=5)
 
-#step 6
-def multiply(a, b=2):
-    print(a * b)
-multiply(5)
+# #step 6
+# def multiply(a, b=2):
+#     print(a * b)
+# multiply(5)
 
-def multiply(a, b=2):
-    print(a * b)
-multiply(5,5)
+# def multiply(a, b=2):
+#     print(a * b)
+# multiply(5,5)
+
+#step 7
+def print_largest(a, b, c):
+    if a > b and a > c:
+        print(a)
+    elif b > a and b > c:
+        print(b)
+    else:
+     print(c)
+print_largest(3,8,5)
+print_largest(7,2,10)
+print_largest(1,4,4)
